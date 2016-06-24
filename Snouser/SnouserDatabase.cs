16eefa851ac7c1d5f050839763b1ac9182705b35
Matt Cordell <matthew.cordell@nehta.gov.au>
2016-06-24 14:58:05 +1000

    add basic helper classes to interogate the db

--- Snouser/SnouserDatabase.cs
+++ Snouser/SnouserDatabase.cs
@@ -4,24 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.Data;
 
 namespace Snouser
 {
     class SnouserDatabase
     {
-        private SQLiteConnection db = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+        private string connString = "Data Source=Snouser.db;Version=3;";
+
+        // Basic db helper classes
 
         private void ExecuteQuery(string txtQuery)
         {
-            using (db.Open())
+            using (SQLiteConnection cnn = new SQLiteConnection(connString))
             {
-                SQLiteCommand cmd = db.CreateCommand();
+                cnn.Open();
+                SQLiteCommand cmd = cnn.CreateCommand();
                 cmd.CommandText = txtQuery;
                 cmd.ExecuteNonQuery();
-            }
-            
+            }            
         }
-            
+
+        private string QueryValue(string txtQuery)
+        {
+            using (SQLiteConnection cnn = new SQLiteConnection(connString))
+            {
+                cnn.Open();
+                SQLiteCommand cmd = cnn.CreateCommand();
+                cmd.CommandText = txtQuery;
+                return cmd.ExecuteScalar().ToString();
+            }
+        }
+
+        private DataTable QueryResultSet(string txtQuery)
+        {
+            DataTable dt = new DataTable();
+
+            using (SQLiteConnection cnn = new SQLiteConnection(connString))
+            {
+                cnn.Open();
+                SQLiteCommand cmd = cnn.CreateCommand();
+                cmd.CommandText = txtQuery;
+                SQLiteDataReader rdr = cmd.ExecuteReader();
+                dt.Load(rdr);
+                rdr.Close();
+            }
+            return dt;
+        }
+
+
+
+
 
     }
 }
